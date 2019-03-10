e9e6f1dc170b0d85f34f06084e4c2e81923271d0
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-03-10 22:14:38 +0300

    Add project files.

---
+++ CoreStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
