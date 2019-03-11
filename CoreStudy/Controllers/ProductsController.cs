e22fe7cf722da7ebcc2a9acd1fd3a7eadae13d4a
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-03-12 00:27:52 +0300

    Task 1. Base site

---
+++ CoreStudy/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoreStudy.Models;
+
+namespace CoreStudy.Controllers
+{
+    public class ProductsController : Controller
+    {
+        #region DI
+        private readonly NorthwindContext db;
+
+        public ProductsController(NorthwindContext context)
+        {
+            db = context;
+        }
+        #endregion
+
+
+        // GET: Products/Index
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            //using Eager Loading (.Include().ThenInclude()...)
+            var allProducts = db.Products.Include(p => p.Category).Include(p => p.Supplier);
+            return View(await allProducts.ToListAsync());
+        }
+    }
+}
