c01920f150d13a220ae1d6496bcc8d4fedcb88ba
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-03-14 00:10:13 +0300

    Task 3. Edit forms and Server-side validation

--- CoreStudy/Models/Products.cs
+++ CoreStudy/Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoreStudy.Models
 {
@@ -11,13 +12,27 @@ namespace CoreStudy.Models
         }
 
         public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(40)]
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
+
+        [StringLength(20)]
         public string QuantityPerUnit { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 1000)]
         public decimal? UnitPrice { get; set; }
+
+        [Range(1,100)]
         public short? UnitsInStock { get; set; }
+
+        [Range(1, 100)]
         public short? UnitsOnOrder { get; set; }
+
+        [Range(1, 100)]
         public short? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
 
