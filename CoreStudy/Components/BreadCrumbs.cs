ba5c3728a416e2595785d0d9fd0be30e396a586a
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-04-07 20:17:35 +0300

    Module 3. Task 2. ViewComponents / PartialViews

---
+++ CoreStudy/Components/BreadCrumbs.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreStudy.Components
+{
+    public class BreadCrumbs: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string controller = RouteData.Values["controller"].ToString();
+            string action = RouteData.Values["action"].ToString();
+            
+            string result = $"<h4> <span style='color: black'> Home > </span> <span style='color: red'> {controller} > </span>";
+            if (action != "Index")
+            {
+                result += $"<span style='color: yellow'> {action} > </span>";
+            }
+            result += $"</h4>";
+
+            return View("Default", new HtmlContentViewComponentResult(new HtmlString(result)));
+        }
+    }
+}
