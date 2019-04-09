2e6e38de8b34d772eb8109bed48e5eeab40ce5e8
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-04-09 20:44:38 +0300

    NorthwindImageLinkHelper added

---
+++ CoreStudy/Helpers/NorthwindImageLinkHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreStudy.Helpers
+{
+    public static class NorthwindImageLinkHelper
+    {
+        //this method creates an Html Helper, which produce link to image in format "images/{image_id}"
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper html, int imageId, string linkText)
+        {
+            return html.RouteLink(linkText, new { controller = "Categories", action = "GetPictureById", id = imageId });
+        }
+    }
+}
