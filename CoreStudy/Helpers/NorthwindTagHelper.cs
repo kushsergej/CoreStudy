c61fc9f938bf9e1fdcaab57925778848eb8976f3
siarhei.kushniaruk <siarhei_kushniaruk@epam.com>
2019-04-11 14:46:33 +0300

    Module 3. Task 3. Html Helpers / Html Tags (Tag Helpers)

---
+++ CoreStudy/Helpers/NorthwindTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreStudy.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindTagHelper: TagHelper
+    {
+        public string NorthwindId { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            output.Attributes.SetAttribute("href", $"images/{NorthwindId}");
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
